beb338045dba26c54dc1dedf80cc6da434119d23
person93 <person93.person93@gmail.com>
2025-04-02 20:00:30 -0400

    Add accessors to `DynamicEnum` for the `DynamicVariant`

--- crates/bevy_reflect/src/enums/dynamic_enum.rs
+++ crates/bevy_reflect/src/enums/dynamic_enum.rs
@@ -139,6 +139,19 @@ impl DynamicEnum {
         self.variant = variant.into();
     }
 
+    pub fn variant(&self) -> &DynamicVariant {
+        &self.variant
+    }
+
+    /// Using the mut reference to switch to a different variant will ___not___ update the
+    /// internal tracking of the variant name and index.
+    ///
+    /// If you want to switch variants, prefer one of the setters:
+    /// [`DynamicEnum::set_variant`] or [`DynamicEnum::set_variant_with_index`].
+    pub fn variant_mut(&mut self) -> &mut DynamicVariant {
+        &mut self.variant
+    }
+
     /// Create a [`DynamicEnum`] from an existing one.
     ///
     /// This is functionally the same as [`DynamicEnum::from_ref`] except it takes an owned value.
